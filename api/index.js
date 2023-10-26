789b6c353ec996ab2caed5d02e8463505dc1e570
rishabh9905 <rishabhpatel9905@gmail.com>
2023-10-26 21:44:14 +0530

    connect mongodb

--- api/index.js
+++ api/index.js
@@ -1,8 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import colors from "colors"
 
+dotenv.config();
+
+mongoose 
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB".bgGreen);  
+  })
+  .catch((err) => {
+    console.log(err);
+  }); 
+  
 const app = express();
-
+ 
 app.listen(3000, () => {
-    console.log(`Server Listening on port 3000`);
-});
-
+  console.log(`Server Listening on port 3000`.bgCyan);
+});  
+    
